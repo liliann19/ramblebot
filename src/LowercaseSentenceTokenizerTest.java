329c12a3b3e23b2a59715fc7e2c5866c08c7aebf
Lilian <210286008+liliann19@users.noreply.github.com>
2025-10-13 10:33:30 -0700

    Wave 2 created test to handle inputs with many spaces

--- src/LowercaseSentenceTokenizerTest.java
+++ src/LowercaseSentenceTokenizerTest.java
@@ -16,9 +16,15 @@ class LowercaseSentenceTokenizerTest {
     }
 
     // Wave 2
-    /*
-     * Write your test here!
-     */
+    @Test 
+    void testTokenizeWithManySpaces() {
+        LowercaseSentenceTokenizer tokenizer = new LowercaseSentenceTokenizer();
+        Scanner scanner = new Scanner("hello   hi hi hi   hello hello");
+        List<String> tokens = tokenizer.tokenize(scanner);
+
+        assertEquals(List.of("hello", "hi", "hi", "hi", "hello", "hello"), tokens);
+
+    }
     
 
     // Wave 3
