4970d364c3dfecc8456465c16add3e2137462e53
Lilian <210286008+liliann19@users.noreply.github.com>
2025-10-14 21:38:05 -0700

    Wave 5 completed predictNextWord

--- src/UnigramWordPredictor.java
+++ src/UnigramWordPredictor.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -107,8 +108,17 @@ public class UnigramWordPredictor implements WordPredictor {
    * @return the predicted next word, or null if no prediction can be made
    */
   public String predictNextWord(List<String> context) {
-    // TODO: Return a predicted word given the words preceding it
     // Hint: only the last word in context should be looked at
+    String lastWordInContext = context.get(context.size() - 1);
+    if (neighborMap.containsKey(lastWordInContext)) {
+      List<String> nextWords = neighborMap.get(lastWordInContext);
+
+      Random rand = new Random();
+      int nextWordSize = nextWords.size();
+      int randomIndex = rand.nextInt(nextWordSize);
+      String predicitedWord = nextWords.get(randomIndex);
+      return predicitedWord;
+    }
     return null;
   }
   
